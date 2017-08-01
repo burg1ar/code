091f49b67ffd35e6f436bb0693e06420bc941ad0
burg1ar <921752421@qq.com>
2017-08-01 11:37:44 +0800

    update:detect missing/redundant operator

--- DataStructure/StackAndQueue/EvalExpression/eval.cpp
+++ DataStructure/StackAndQueue/EvalExpression/eval.cpp
@@ -18,16 +18,20 @@ int priority(char ch){
     }
 }
 //shunting-yard algorithm
+//update:detect missing operator,add "digCount" and "opCount"
 string infix_to_postfix(string expr){
     stack<char> stk;
+    int digCount=0,opCount=0;
     string output="";
     for(auto it=expr.begin();it!=expr.end();++it){
         if(isDigit(*it)){
             output.push_back(*it);
+            ++digCount;
         }else if(isOperator(*it)){
             while(!stk.empty() && isOperator(stk.top())
                     && priority(*it)<=priority(stk.top())){
                 output.push_back(stk.top());
+                ++opCount;
                 stk.pop();
             }
             stk.push(*it);
@@ -36,6 +40,7 @@ string infix_to_postfix(string expr){
         }else if(*it==')'){
             while(!stk.empty() && stk.top()!='('){
                 output.push_back(stk.top());
+                ++opCount;
                 stk.pop();
             }
             if(stk.empty()){
@@ -56,15 +61,51 @@ string infix_to_postfix(string expr){
             return "parenthesis dismatch";
         }
         output.push_back(stk.top());
+        ++opCount;
         stk.pop();
     }
-    return output;
+    if(digCount-opCount>1){
+        return "missing operator";
+    }else if(digCount-opCount<1){
+        return "redundant operator";
+    }else{
+        return output;
+    }
+}
+double eval(string expr){
+    stack<double> stk;
+    double x,y,z;
+    for(auto it=expr.begin();it!=expr.end();++it){
+        if(isDigit(*it)){
+            stk.push(*it-'0');
+        }else if(isOperator(*it)){
+            x=stk.top();stk.pop();
+            y=stk.top();stk.pop();
+            switch(*it){
+                case '+':
+                    z=y+x;break;
+                case '-':
+                    z=y-x;break;
+                case '*':
+                    z=y*x;break;
+                case '/':
+                    z=y/x;break;
+            }
+            stk.push(z);
+        }else{
+            cout<<expr<<endl;
+            return 0; //ERROR
+        }
+    }
+    return stk.top();
 }
 int main(){
-    string expr;
+    string expr,temp;
     while(cin>>expr){
         cout<<"infix notation: "<<expr<<endl;
-        cout<<"postfix notation: "<<infix_to_postfix(expr)<<endl;
+        temp=infix_to_postfix(expr);
+        cout<<"postfix notation: "<<temp<<endl;
+        cout<<eval(temp)<<endl;
     }
     return 0;
 }
