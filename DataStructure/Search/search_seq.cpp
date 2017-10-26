56dfffb79594d1ffb218d3d7197ff08ad708e35e
burg1ar <921752421@qq.com>
2017-10-26 11:15:48 +0800

    update search_seq

--- DataStructure/Search/search_seq.cpp
+++ DataStructure/Search/search_seq.cpp
@@ -6,9 +6,21 @@ int search_seq(int arr[],int n,int key){
     if(i==n) return -1;
     return i;
 }
+int search_sorted_seq(int arr[],int n,int key){
+    int i;
+    for(i=0;i<n && arr[i]<key;i++);
+    if(i<n && arr[i]==key)
+        return i;
+    else if(i-1<n && arr[i-1]==key)
+        return i-1;
+    else
+        return -1;
+}
 int main(){
     int arr[]={1,2,3,5,8,13,21};
     cout<<search_seq(arr,7,8)<<endl;
     cout<<search_seq(arr,7,7)<<endl;
+    cout<<search_sorted_seq(arr,7,8)<<endl;
+    cout<<search_sorted_seq(arr,7,7)<<endl;
     return 0;
 }
